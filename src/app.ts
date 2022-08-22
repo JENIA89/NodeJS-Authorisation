50e99c4ff5148498eae01259b859d0f8be93d391
Eugene <eugene.popolamov@effective-soft.com>
2022-08-22 09:26:50 +0300

    feat: add logger

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express, { Express } from 'express';
 import { Server } from 'http';
+import { LoggerService } from './logger/logger.service';
 
 export class App {
   app: Express;
   server: Server;
   port: number;
+  logger: LoggerService;
 
-  constructor() {
+  constructor(logger: LoggerService) {
     this.app = express();
     this.port = 8000;
+    this.logger = logger;
   }
 
   useRoutes() {
@@ -20,6 +23,6 @@ export class App {
   public async init() {
     this.useRoutes();
     this.server = this.app.listen(this.port);
-    console.log(`Server is running on ${this.port}`);
+    this.logger.log(`Server is running on ${this.port}`);
   }
 }

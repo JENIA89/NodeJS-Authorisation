a807ae2849f908d5a20ee29cdf2338894a910894
Eugene <eugene.popolamov@effective-soft.com>
2022-08-27 23:19:44 +0300

    feat: depete import

--- src/users/users.service.interface.ts
+++ src/users/users.service.interface.ts
@@ -1,4 +1,3 @@
-import { NextFunction, Response, Request } from 'express';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
 import { User } from './user.entity';

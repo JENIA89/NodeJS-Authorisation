4bdc82160344ac6b777d0c8a41664c159c62cd65
Eugene <eugene.popolamov@effective-soft.com>
2022-09-07 22:28:33 +0300

    feat: update userController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -71,7 +71,8 @@ export class UserController extends BaseController implements IUsersController {
   }
 
   async info({ user }: Request, res: Response, next: NextFunction): Promise<void> {
-    this.ok(res, { email: user });
+    const userInfo = await this.userService.getUserInfo(user);
+    this.ok(res, { email: userInfo?.email, id: userInfo?.id });
   }
 
   private signJWT(email: string, secret: string): Promise<string> {
